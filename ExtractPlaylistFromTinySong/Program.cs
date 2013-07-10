7f59070f569ce083755d5d6257513c0832068253
Andrey Rjeutski <filhit@gmail.com>
2013-07-11 02:48:12 +0300

    Initial commit.

---
+++ ExtractPlaylistFromTinySong/Program.cs
@@ -0,0 +1,48 @@
+﻿using RestSharp;
+using RestSharp.Deserializers;
+using RestSharp.Serializers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractPlaylistFromTinySong
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("first parameter is tinysong api key, second parameter is file name with queries");
+                return;
+            }
+
+            var client = new RestClient("http://tinysong.com/");
+            client.AddHandler("text/html", new JsonDeserializer());
+            var results = new List<Song>();
+            var queries = File.ReadAllLines(args[1]);
+            foreach (var query in queries)
+            {
+                results.AddRange(GetSongs(args[0], client, query));
+            }
+
+            var jsonSerializer = new JsonSerializer();
+            Console.WriteLine(jsonSerializer.Serialize(results));
+        }
+
+        private static IEnumerable<Song> GetSongs(string apiKey, IRestClient client, string query)
+        {
+            var request = new RestRequest("s/{query}", Method.GET);
+            request.AddParameter("format", "json"); 
+            request.AddParameter("limit", 32);
+            request.AddParameter("key", apiKey);
+            request.AddUrlSegment("query", "Gogol Bordello"); 
+                        
+            var response = client.Execute<List<Song>>(request);
+            return response.Data;
+        }
+    }
+}
---
+++ ExtractPlaylistFromTinySong/Song.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractPlaylistFromTinySong
+{
+    public class Song
+    {
+        public string Url { get; set; }
+
+        public int SongID { get; set; }
+
+        public string SongName { get; set; }
+
+        public int ArtistID { get; set; }
+
+        public string ArtistName { get; set; }
+
+        public int AlbumID { get; set; }
+
+        public string AlbumName { get; set; }
+    }
+}
